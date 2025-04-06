e6baedf0e94f3c04cedbacde1445494494a02314
samthgit <stineoherrera0@gmail.com>
2025-04-06 16:28:59 +0200

    Añadidos parte front-end y back-end del proyecto

---
+++ chuok-backend/src/main/java/com/chuok/backend/ChuokBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chuok.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuokBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuokBackendApplication.class, args);
+	}
+
+}
---
+++ chuok-backend/src/test/java/com/chuok/backend/ChuokBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuok.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuokBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
